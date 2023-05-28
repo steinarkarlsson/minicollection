9e6b4d2f91e8d8c3e5ef8aa60700149e97ec3864
Steinar Karlsson <steinarkarlsson@gmail.com>
2023-05-28 19:00:34 +1200

    Change logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,11 +24,7 @@ function Header() {
 
     return <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
-            <Image src="/ministock-logo.png"
-                   width={100}
-                   height={100}
-                   alt="Mini Stock Logo"
-                   className="cursor-pointer object-contain"/>
+            <p>Mini Collection</p>
 
             <ul className="hidden space-x-4 md:flex">
                 <li className="headerLink">Home</li>
