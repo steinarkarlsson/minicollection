55c30c0ef5f16fd969909496740b2685b6aa1a73
Steinar Karlsson <steinarkarlsson@gmail.com>
2023-05-29 20:08:44 +1200

    Remove Chevrons

--- components/MiniCardGrid.tsx
+++ components/MiniCardGrid.tsx
@@ -1,5 +1,4 @@
 import {Figure} from "../typings";
-import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/react/24/outline";
 import MiniCard from "./MiniCard";
 
 interface Props {
@@ -11,13 +10,11 @@ function MiniCardGrid({title,figures}:Props) {
         <div className="h-22 space-y-0.5 md:space-y-2 pt-16">
             <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">{title}</h2>
             <div className="group relative md:-ml-1">
-                <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 cursor-pointer transition hover:scale-125 group-hover:opacity-100"/>
                 <div className="flex flex-wrap items-center space-x-0.5 md:space-x-2.5 md:p-2">
                     {figures.map((figure) => (
                         <MiniCard key={figure.mainName} figure={figure}/>
                     ))}
                 </div>
-                <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 cursor-pointer transition hover:scale-125 group-hover:opacity-100"/>
             </div>
         </div>
     )
