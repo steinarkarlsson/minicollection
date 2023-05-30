c461771122f942ba9c17212b5711ac73461adf37
Steinar Karlsson <steinarkarlsson@gmail.com>
2023-05-30 19:01:30 +1200

    Remove grid border

--- components/MiniCardGrid.tsx
+++ components/MiniCardGrid.tsx
@@ -9,7 +9,7 @@ function MiniCardGrid({title,figures}:Props) {
         <div className="space-y-0.5 md:space-y-2 pt-16">
             <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">{title}</h2>
             <div className="group relative md:-ml-1">
-                <div className="border-2 border-white-1000 flex flex-wrap space-x-0.5 md:space-x-2.5 md:p-2">
+                <div className="flex flex-wrap space-x-0.5 md:space-x-2.5 md:p-2">
                     {figures.map((figure) => (
                             <MiniCard
                                 key={figure.mainName}
