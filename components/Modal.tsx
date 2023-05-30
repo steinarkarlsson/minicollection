26fd28a6c0edbdfaad50aa2a88bfc77d7a60b82e
Steinar Karlsson <steinarkarlsson@gmail.com>
2023-05-30 19:00:55 +1200

    Add modal onClick and onClose

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,21 @@
 import MuiModal from '@mui/material/Modal';
-import {modalState} from "../atoms/modalAtom";
-import {useRecoilValue} from "recoil";
+import {figureState, modalState} from "../atoms/modalAtom";
+import {useRecoilState} from "recoil";
+import {XMarkIcon} from "@heroicons/react/20/solid";
 function Modal(){
-    const showModal = useRecoilValue(modalState)
+    const [showModal, setShowModal] = useRecoilState(modalState)
+    const handleClose = () => {
+        setShowModal(false)
+    }
 
-    return <MuiModal open={showModal}>
+    return <MuiModal open={showModal} onClose={handleClose}>
         <>
-            Modal
+            <button onClick={handleClose} className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818]">
+                <XMarkIcon className="h-6 w-6"/>
+            </button>
+            <p>Hello There</p>
         </>
     </MuiModal>
-
 }
 
 export default Modal
