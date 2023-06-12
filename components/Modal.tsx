75f40da19931fc6234903764d0e0ea077291bfa3
Steinar Karlsson <steinarkarlsson@gmail.com>
2023-06-12 19:59:45 +1200

    Integrate Sanity CMS

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,19 +1,24 @@
 import MuiModal from '@mui/material/Modal';
-import {figureState, modalState} from "../atoms/modalAtom";
+import {modalState} from "../atoms/modalAtom";
 import {useRecoilState} from "recoil";
 import {XMarkIcon} from "@heroicons/react/20/solid";
-function Modal(){
+
+function Modal() {
     const [showModal, setShowModal] = useRecoilState(modalState)
+
     const handleClose = () => {
         setShowModal(false)
     }
 
     return <MuiModal open={showModal} onClose={handleClose}>
         <>
-            <button onClick={handleClose} className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818]">
+            <button onClick={handleClose}
+                    className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]">
                 <XMarkIcon className="h-6 w-6"/>
             </button>
-            <p>Hello There</p>
+            <div>
+
+            </div>
         </>
     </MuiModal>
 }
