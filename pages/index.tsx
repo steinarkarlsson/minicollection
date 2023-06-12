23717b5ed8d0eabc3d8e763f9d147a5898eec45f
Steinar Karlsson <steinarkarlsson@gmail.com>
2023-06-12 19:59:45 +1200

    Integrate Sanity CMS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import Head from 'next/head'
 import Header from "../components/Header";
 import MiniCardGrid from "../components/MiniCardGrid";
-import connectToDatabase from '../db'
 import {Figure} from "../typings";
 import {useRecoilValue} from "recoil";
 import {modalState} from "../atoms/modalAtom";
 import React from "react";
 import Modal from "../components/Modal";
+import {createClient} from "next-sanity";
 
 interface Props {
     figures: Figure[]
 }
 
-const Home = ({ figures }: Props) => {
+const Home = ({figures}: Props) => {
 
     const showModal = useRecoilValue(modalState)
 
@@ -23,23 +23,31 @@ const Home = ({ figures }: Props) => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <Header/>
-                <main className="relative pl-4 pb-24  lg:space-y-24 lg:pl-16 mt-10">
-                    <section className="md:space-y-24 mt-10">
-                        <MiniCardGrid title={"Miniatures"} figures={figures}/>
-                    </section>
-                </main>
+            <main className="relative pl-4 pb-24  lg:space-y-24 lg:pl-16 mt-10">
+                <section className="md:space-y-24 mt-10">
+                    <MiniCardGrid title={"Miniatures"} figures={figures}/>
+                </section>
+            </main>
             {showModal && <Modal/>}
         </div>
     )
 }
 export default Home
 
-export async function getServerSideProps() {
-    const db = await connectToDatabase()
-    const figures = await db.collection('figures').find({}).toArray()
+const client = createClient(
+    {
+        projectId: process.env.SANITY_PROJECTID,
+        dataset: process.env.SANITY_DATASET,
+        apiVersion: "2023-06-12",
+        useCdn: false
+    }
+);
+
+export async function getStaticProps() {
+    const figures = await client.fetch(`*[_type == "figure"]`);
     return {
         props: {
-            figures: JSON.parse(JSON.stringify(figures))
+            figures
         }
-    }
+    };
 }
