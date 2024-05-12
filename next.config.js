4e297588a9d206bbb540d7ca4913385740825480
steinarkarlsson <steinarkarlsson@gmail.com>
2024-05-12 17:33:18 +1200

    Update depreciated domains to remotePatterns in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.sanity.io'],
+    remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.sanity.io',
+          port: '',
+        }
+      ],
   }
 }
