91bdef9d7366493c9531c278e8a9f22c79351ee9
steinarkarlsson <steinarkarlsson@gmail.com>
2024-05-21 10:32:39 +1200

    Add Sanity public variables to client

--- lib/client.ts
+++ lib/client.ts
@@ -2,8 +2,8 @@ import {createClient} from "next-sanity";
 
 export const client = createClient(
     {
-        projectId: process.env.NEXT_PUBLIC_SANITY_PROJECTID as string,
-        dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
+        projectId: '4llymfg7',
+        dataset: 'production',
         apiVersion: "2023-06-12",
         useCdn: false
     }
