82ad1c3bba5ead8b7f413f32db945e3d3e79cf63
steinarkarlsson <steinarkarlsson@gmail.com>
2024-05-21 10:11:51 +1200

    Upgrade to NextJS 14.2, React 18.3 and React-dom 18.3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {RecoilRoot} from "recoil";
-import React from "react";
+import React from 'react';
 
-function MyApp({Component, pageProps}: AppProps) {
+const MyApp = ({Component, pageProps}: AppProps) => {
     return (
         <RecoilRoot>
             <Component {...pageProps} />
