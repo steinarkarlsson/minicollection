aa1e1a9b8a95e41478b1548b049e15391f691bf8
steinarkarlsson <steinarkarlsson@gmail.com>
2024-05-22 20:26:08 +1200

    Added Speed Insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {RecoilRoot} from "recoil";
 import React from 'react';
+import {SpeedInsights} from "@vercel/speed-insights/next"
 
 const MyApp = ({Component, pageProps}: AppProps) => {
     return (
         <RecoilRoot>
-            <Component {...pageProps} />
+            <SpeedInsights>
+                <Component {...pageProps} />
+            </SpeedInsights>
         </RecoilRoot>
     )
 }
