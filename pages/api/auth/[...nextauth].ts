06a0927831882d0fc63fef95ec4e2cbea653e416
steinarkarlsson <steinarkarlsson@gmail.com>
2024-05-22 20:39:07 +1200

    Fix NextAuth implementation

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,14 @@
 import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
     providers: [
-        Providers.Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
         // Add other providers as needed
     ],
-    database: process.env.DATABASE_URL,
+    //database: process.env.DATABASE_URL,
     // Add additional NextAuth configuration as needed
 });
