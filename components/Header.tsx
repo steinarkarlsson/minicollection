062864de9fff9a0d81a1b7a828c6af93d68a7bae
steinarkarlsson <steinarkarlsson@gmail.com>
2024-05-22 19:55:54 +1200

    Improved header styling for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,12 +20,12 @@ function Header() {
         }
     }, [])
 
-    return <header className={`flex justify-center ${isScrolled && 'bg-[#141414]'}`}>
+    return <header className={`flex justify-center ${isScrolled && 'bg-[#141414] p-1'}`}>
         <div className="flex items-center">
-            <Image src="/icon.png" width="60" height="60" alt="logo"/>
+            <Image src="/icon.png" width="60" height="60" alt="logo" className="w-10 lg:w-20"/>
             <div className="flex flex-col">
-            <p className="text-2xl">Mini Collection</p>
-            <p className="">A Strategy Battle Game Collectors Archive</p>
+            <p className="text-md lg:text-2xl">Mini Collection</p>
+            <p className="text-xs lg:text-md">A Strategy Battle Game Collectors Archive</p>
             </div>
         </div>
     </header>
