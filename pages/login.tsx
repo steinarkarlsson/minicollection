9f05e0ead58efe1981244e4035f1ffa051e6af17
steinarkarlsson <steinarkarlsson@gmail.com>
2024-05-25 22:45:23 +1200

    Add login requirement to access app

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,65 @@
-function Login(){
-    return <div>Login</div>
-}
+'use client';
+import {supabase} from "../lib/supabase";
+import {useState} from "react";
+import {useRouter} from "next/navigation";
 
-export default Login
+export default function Login() {
+    const [data, setData] = useState<{
+        email: '',
+        password: ''
+    }>({
+        email: '',
+        password: ''
+    })
+
+    const router = useRouter();
+
+    const login = async () => {
+        try {
+            let {data: dataUser} = await supabase
+                .auth
+                .signInWithPassword({
+                    email: data.email,
+                    password: data.password,
+                })
+            if (dataUser) {
+                router.refresh();
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleChange = (e: any) => {
+        const {name, value} = e.target;
+        setData((prev: any) => ({
+            ...prev,
+            [name]: value,
+        }));
+    }
+    return <div className='container mx-auto w-[400px]'>
+        <div className='grid'>
+            <label>Email</label>
+            <input
+                type='text'
+                name='email'
+                className='text-black'
+                value={data?.email}
+                onChange={handleChange}
+            />
+        </div>
+        <div className='grid'>
+            <label>Password</label>
+            <input
+                type='password'
+                name='password'
+                className='text-black'
+                value={data?.password}
+                onChange={handleChange}
+            />
+        </div>
+        <div>
+            <button onClick={login}>Login</button>
+        </div>
+    </div>
+}
