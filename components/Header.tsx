6d2b8ac43e9dc4f63194be54bfb02bbf1c6644ff
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-10 15:29:26 +1200

    Add buy me a coffee button

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,6 +28,19 @@ function Header() {
                 <p className="text-xs lg:text-md">A Strategy Battle Game Collectors Archive</p>
             </div>
         </div>
+        <div className="flex align-right ml-auto m-2">
+            <div className="mx-auto">
+                <div className="mx-auto text-center">
+                    <div className="mt-4">
+                        <img className="mx-auto h-12 transition delay-75 ease-in-out hover:scale-110"
+                             src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+                             alt="Buy Me a Coffee"
+                             onClick={() => window.open('https://buymeacoffee.com/steinarkaro')}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
 }
 
