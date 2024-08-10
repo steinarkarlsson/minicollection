d197f4b786cb5406e27fa4e40f3f522d2e650f46
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-10 14:42:50 +1200

    Add infinite scroll to Figure grid

--- components/MiniCardGrid.tsx
+++ components/MiniCardGrid.tsx
@@ -1,24 +1,58 @@
 import {GridFigure} from "../typings";
 import MiniCard from "./MiniCard";
+import {useEffect, useState} from 'react';
+import {getFigureGridInfo} from '../lib/sanityQueries';
 
 interface Props {
     figures: GridFigure[]
 }
 
+function useScrollToEnd(callback: () => void) {
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentPosition = window.scrollY;
+            const botttomPosition = document.documentElement.scrollHeight - window.innerHeight - 50;
+
+            if (currentPosition >= botttomPosition) {
+                callback()
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
+}
+
+
 function MiniCardGrid({figures}: Props) {
+    const [displayedFigures, setDisplayedFigures] = useState<GridFigure[]>([])
+    const [count, setCount] = useState<number>(32)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        setIsLoading(true)
+        getFigureGridInfo('', '', '', count).then((figures) => {
+            setDisplayedFigures(figures)
+            setIsLoading(false)
+        })
+    }, [count]);
+
+    useScrollToEnd(() => {
+        setIsLoading(true)
+        setCount(prevCount => prevCount + 32)
+    })
+
     return (
-        <>
-                <div className="flex">
                     <div className="flex flex-wrap justify-center">
-                        {figures.map((figure) => (
-                            (<MiniCard
-                                key={`${figure.mainName}-${figure.releaseWave?.name}`}
-                                figure={figure}
-                            />)
+                        <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'><p>{isLoading ? 'IsLoading': 'Not loading' }</p></div>
+                        {displayedFigures.map((figure) => (
+                            <MiniCard figure={figure} key={
+                                figure.mainName + figure.releaseWave?.name
+                            }/>
                         ))}
                     </div>
-                </div>
-        </>
     )
 }
 
