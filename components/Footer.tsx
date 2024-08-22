76b6bf957770471227a3042f0d873f229837cb61
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-22 15:29:25 +1200

    Added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className='flex flex-row w-full space-x-10 justify-center border-2'>
+            <a href='/contact'>Contact</a>
+            <a href='/privacy'>Privacy Policy</a>
+            <a href='/copyright'>Copyright</a>
+        </footer>
+    );
+}
+
+export default Footer;
