aa1f41bda8d129473325239b9ad96cb19be84925
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-22 14:10:01 +1200

    Refactored Header icon into component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Image from "next/image";
 import CoffeeButton from "./CoffeeButton";
-
+import MiniCollectionIcon from "./MiniCollectionIcon";
 function Header() {
     return (
-        <header className='fixed top-0 w-full h-20 flex flex-row justify-center items-center bg-black'>
-            <Image src="/icon.png" width="60" height="60" alt="logo" className="w-10 lg:w-20"/>
-            <div className="flex flex-col">
-                <p className="text-md lg:text-2xl">Mini Collection</p>
-                <p className="text-xs lg:text-md">A Strategy Battle Game Collectors Archive</p>
-            </div>
+        <header className='fixed top-0 w-full h-20 flex flex-row justify-center items-center bg-black border-2'>
+            <MiniCollectionIcon/>
             <CoffeeButton/>
         </header>
     )
---
+++ components/header/MiniCollectionIcon.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+function MiniCollectionIcon() {
+    return (
+        <div className='flex flex-row border-2'>
+            <Image src="/icon.png" width="60" height="60" alt="logo" className="w-10 lg:w-20"/>
+            <div className="flex flex-col">
+                <p className="text-md lg:text-2xl">Mini Collection</p>
+                <p className="text-xs lg:text-md">A Strategy Battle Game Collectors Archive</p>
+            </div>
+        </div>
+    )
+}
+
+export default MiniCollectionIcon
