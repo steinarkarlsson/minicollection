421a6b5641c6f1d9204e42ead8fa854d8c4d2472
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-23 19:48:27 +1200

    Added Clear Filters button and styled search filters

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-import { useDebounce }  from 'use-debounce'
+import {useEffect, useRef, useState} from 'react'
+import {useRouter} from 'next/navigation'
+import {useDebounce} from 'use-debounce'
 
-const Search = ({ searchFilter }: { searchFilter?: string }) => {
+const Search = ({searchFilter}: { searchFilter?: string }) => {
     const router = useRouter()
     const initialRender = useRef(true)
 
@@ -21,25 +20,18 @@ const Search = ({ searchFilter }: { searchFilter?: string }) => {
         if (!query) {
             router.push(`/`)
         } else {
-            router.push(`/?searchFilter=${query}`)
+            router.replace(`/?searchFilter=${query}`)
         }
     }, [query])
 
+
     return (
-        <div className='relative rounded-md shadow-sm'>
-            <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
-                <MagnifyingGlassIcon
-                    className='h-5 w-5 text-gray-400'
-                    aria-hidden='true'
-                />
-            </div>
-            <input
-                value={text}
-                placeholder='Search Miniatures...'
-                onChange={e => setText(e.target.value)}
-                className='block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6'
-            />
-        </div>
+        <input
+            value={text}
+            placeholder='Search Miniatures...'
+            onChange={e => setText(e.target.value)}
+            className='flex bg-gray-800 w-full h-12 text-lg rounded-md scrollbar scrollbar-thumb-gray-500 scrollbar-track-gray-800 scrollbar-thumb-rounded-full scrollbar-track-rounded-full hover:bg-gray-700 transition duration-200'
+        />
     )
 }
 
