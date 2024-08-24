c9158f7a7ab8adcf4602af2206c696bc04bea9e6
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-25 09:51:37 +1200

    Remove scaffold borders

---
+++ components/searchBar/SearchBar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Faction, ReleaseWave} from "../../typings";
+import Search from "./Search";
+import {Select} from "@mui/material";
+import Link from "next/link";
+import {ClearFilters} from "./ClearFilters";
+import {FactionSelect} from "./FactionSelect";
+import {ReleaseWaveSelect} from "./ReleaseWaveSelect";
+
+interface searchBarProps {
+    factions: Faction[],
+    releaseWaves: ReleaseWave[],
+    searchFilter: string | undefined,
+    factionFilter: string | undefined,
+    releaseWaveFilter: string | undefined,
+}
+
+export function SearchBar({
+                              searchFilter,
+                              factions,
+                              releaseWaves,
+                              factionFilter,
+                              releaseWaveFilter,
+                          }: searchBarProps) {
+
+    return (
+        <div
+            className='flex flex-col lg:flex-row justify-center items-center p-2 space-y-4 lg:space-y-0 lg:space-x-10 w-full xl:w-2/3'>
+            <Search searchFilter={searchFilter}/>
+            <FactionSelect filterObject={factions} searchFilter={searchFilter} factionFilter={factionFilter} releaseWaveFilter={releaseWaveFilter}/>
+            <ReleaseWaveSelect filterObject={releaseWaves} searchFilter={searchFilter} factionFilter={factionFilter} releaseWaveFilter={releaseWaveFilter}/>
+            <ClearFilters/>
+        </div>
+    )
+}
+
