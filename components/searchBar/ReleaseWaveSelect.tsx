842e1970372becf9988aa5ccc2d686eacf7f9283
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-25 10:57:44 +1200

    Updated SearchBar to have autocomplete select

--- components/searchBar/ReleaseWaveSelect.tsx
+++ components/searchBar/ReleaseWaveSelect.tsx
@@ -1,42 +1,44 @@
-import { MenuItem, Select } from "@mui/material";
-import Link from "next/link";
+'use client'
+
+import {Autocomplete, TextField} from "@mui/material";
 import React from "react";
-import { ReleaseWave } from "../../typings";
+import {ReleaseWave} from "../../typings";
+import {useRouter} from "next/navigation";
 
 interface ReleaseWaveSelectProps {
     filterObject: ReleaseWave[],
     searchFilter: string | undefined,
     factionFilter: string | undefined,
-    releaseWaveFilter: string | undefined,
 }
 
-export function ReleaseWaveSelect({ filterObject, searchFilter, factionFilter, releaseWaveFilter }: ReleaseWaveSelectProps) {
+export function ReleaseWaveSelect({filterObject, searchFilter, factionFilter}: ReleaseWaveSelectProps) {
+
+    const options = filterObject.map(e => ({label: e.name, id: e._id}));
+
+    const router = useRouter();
+
+    const query = `?${searchFilter ? `searchFilter=${searchFilter}` : ''}${factionFilter ? `&factionFilter=${factionFilter}` : ''}`;
+
+    const handleChange = (event: any, value: any) => {
+        if (value) {
+            const prefix = searchFilter || factionFilter ? '&' : ''
+            router.push(`${query}${prefix}releaseWaveFilter=${value.label}`);
+        } else {
+            router.push(query);
+        }
+    };
+    console.log(filterObject)
 
     return (
-        <Select
-            variant='filled'
-            label='releaseWaveFilter'
-            className='flex pb-3 align-middle items-middle bg-gray-800 text-white w-full h-12 text-lg rounded-md scrollbar scrollbar-thumb-gray-500 scrollbar-track-gray-800 scrollbar-thumb-rounded-full scrollbar-track-rounded-full hover:bg-gray-700 transition duration-200'
-        >
-            {filterObject.map((e) => (
-                <MenuItem
-                    value={e.name || ''}
-                    key={e.name}
-                >
-                    <Link
-                        href={{
-                            pathname: '/',
-                            query: {
-                                ...(searchFilter ? { searchFilter } : {}),
-                                ...(factionFilter ? { factionFilter } : {}),
-                                releaseWaveFilter: `${e.name}`,
-                            }
-                        }}
-                    >
-                        {e.name}
-                    </Link>
-                </MenuItem>
-            ))}
-        </Select>
+        <Autocomplete
+            className='bg-gray-800 text-white w-full h-12 text-lg rounded-md scrollbar scrollbar-thumb-gray-500 scrollbar-track-gray-800 scrollbar-thumb-rounded-full scrollbar-track-rounded-full hover:bg-gray-700 transition duration-200'
+            disablePortal
+            id="releaseWaveFilter"
+            defaultValue={undefined}
+            options={options}
+            sx={{width: 300, input: {color: 'white'}, label: {color: 'gray'}}}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} label="Release Wave"/>}
+        />
     )
 }
