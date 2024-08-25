61d8815e413c7ec6ec387ffb0a2c48997f3d93aa
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-25 14:21:00 +1200

    Added generic Grid and ItemCard components and refactored home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,16 @@
-import MiniCardGrid from "../components/MiniCardGrid";
-import {getFactions, getFigureGridInfo, getReleaseWaves} from "../lib/sanityQueries";
+import {getFigureGridInfo, getSets} from "../lib/sanityQueries";
 import Welcome from '../components/Welcome';
-import {SearchBar} from "../components/searchBar/SearchBar";
+import Carousel from "../components/Carousel";
 
-export default async function Home({searchParams}: {
-    searchParams: { [key: string]: string | string[] | undefined }
-}) {
-    const searchFilter = typeof searchParams.searchFilter === 'string' ? searchParams.searchFilter : undefined;
-    const factionFilter = typeof searchParams.factionFilter === 'string' ? searchParams.factionFilter : undefined;
-    const releaseWaveFilter = typeof searchParams.releaseWaveFilter === 'string' ? searchParams.releaseWaveFilter : undefined;
-
+export default async function Home() {
     const figures = await getFigureGridInfo();
-    const factions = await getFactions();
-    const releaseWaves = await getReleaseWaves();
+    const sets = await getSets();
 
     return (
         <div className="flex flex-col items-center mt-10 space-y-2 md:space-y-5 p-4">
             <Welcome/>
-            <SearchBar
-                searchFilter={searchFilter}
-                factionFilter={factionFilter}
-                releaseWaveFilter={releaseWaveFilter}
-                factions={factions}
-                releaseWaves={releaseWaves}
-            />
-            <div className='flex flex-col'>
-                <MiniCardGrid
-                    figures={figures}
-                    searchFilter={searchFilter || ''}
-                    factionFilter={factionFilter || ''}
-                    releaseWaveFilter={releaseWaveFilter || ''}
-                    factions={factions}
-                    releaseWaves={releaseWaves}
-                />
-            </div>
+            <Carousel items={figures} type={'figure'}/>
+            <Carousel items={sets} type={'set'}/>
         </div>
     );
 }
