57a8210288582b332835d7c4c09b2ac5ca3bca0e
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-27 20:21:44 +1200

    Remove Header Menu and Header New

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import {SpeedInsights} from "@vercel/speed-insights/next";
 import {Analytics} from "@vercel/analytics/react"
 import React from "react";
 import RecoilRootWrapper from "./RecoilRootWrapper";
-import HeaderNew from "../components/header/HeaderNew";
+import Header from "../components/header/Header";
 
 export const metadata = {
     title: 'Mini Collection',
@@ -20,7 +20,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         <body>
         <RecoilRootWrapper>
             <div className='flex flex-col min-h-screen justify-between bg-gradient-to-b lg:h-[140vh] !h-screen'>
-                <HeaderNew/>
+                <Header/>
                 <main
                     className="flex-grow mt-20 s lg:mx-48 lg:mx-3/4 space-y-2 md:space-y-5 p-4 lg:space-y-20">
                     {children}
