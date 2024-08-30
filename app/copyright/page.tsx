b0398953cfa51acacf688461b7a67bdaca5a1c58
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-30 18:35:07 +1200

    Added copyright disclaimer

--- app/copyright/page.tsx
+++ app/copyright/page.tsx
@@ -1,7 +1,27 @@
 export default function Copyright() {
     return (
-        <div>
-            <h1>Copyright</h1>
+        <div className='flex flex-col justify-center space-y-5'>
+            <div className='text-2xl'>Copyright Disclaimer</div>
+            <div>
+                minicollection.app is a fan-created database for collectors of Middle-earth Strategy Battle Game
+                miniatures.
+            </div>
+            <div>
+                The images and characters featured on this website are the intellectual property of Games
+                Workshop and the respective rights holders for "The Lord of the Rings" and "The Hobbit" films and books.
+            </div>
+            <div>
+                We do not claim ownership of any of the images or characters displayed on this site. All trademarks,
+                logos, and brand names are the property of their respective owners.
+            </div>
+            <div>
+                They are used on this site solely for informational and reference purposes, to support the collecting
+                community and to provide a comprehensive resource for fellow enthusiasts.s
+            </div>
+            <div>
+                This site is non-commercial and is intended for fan use only. We are not affiliated with, endorsed by,
+                or sponsored by Games Workshop, New Line Cinema, Warner Bros., or any other official entity.
+            </div>
         </div>
     )
 }
