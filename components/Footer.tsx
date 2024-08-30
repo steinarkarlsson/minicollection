3d569fa677c3b12526eade69f1ddadd0631d3b58
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-30 18:36:21 +1200

    Hidden privacy link until OAuth is implemented

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 function Footer() {
     return (
-        <footer className='flex flex-row w-full space-x-10 justify-center border-2'>
+        <footer className='flex flex-row w-full space-x-10 pb-5 justify-center'>
             <Link href='/contact'>Contact</Link>
-            <Link href='/privacy'>Privacy Policy</Link>
-            <Link href='/copyright'>Copyright</Link>
+            {/*<Link href='/privacy'>Privacy Policy</Link>*/}
+            <Link href='/copyright'>Copyright Disclaimer</Link>
         </footer>
     );
 }
