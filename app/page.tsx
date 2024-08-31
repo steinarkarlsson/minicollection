fb7d8cc4c4ca0fce7d16202a25b48211ce25aa71
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-31 16:14:11 +1200

    Added featured API calls for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
-import {getFigureGridInfo, getSets} from "../lib/sanityQueries";
+import {getFeaturedFigures, getFeaturedSets} from "../lib/sanityQueries";
 import Welcome from '../components/Welcome';
 import Carousel from "../components/Carousel";
 
 export default async function Home() {
-    const figures = await getFigureGridInfo();
-    const sets = await getSets();
 
+    const featuredFigures = await getFeaturedFigures();
+    const featuredSets = await getFeaturedSets();
+
+    console.log(featuredSets)
+    console.log(featuredFigures)
     return (
         <div className="flex flex-col items-center space-y-2 md:space-y-5 p-4">
             <Welcome/>
             <div className="flex flex-col">
-                <Carousel items={figures} type={'figure'}/>
-                <Carousel items={sets} type={'set'}/>
+                <Carousel items={featuredFigures} type={'figure'}/>
+                <Carousel items={featuredSets} type={'set'}/>
             </div>
         </div>
     );
